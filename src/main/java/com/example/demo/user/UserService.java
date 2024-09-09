61cfdc3781d5d822b72744d43c2ccdd1e3217736
Efehan Ömer Cekic <Efehan.Cekic@Student.HTW-Berlin.de>
2024-09-09 15:34:08 +0200

    mail will be sent when registered

--- src/main/java/com/example/demo/user/UserService.java
+++ src/main/java/com/example/demo/user/UserService.java
@@ -2,6 +2,7 @@ package com.example.demo.user;
 
 import com.example.demo.jwt.JwtTokenUtil;
 import com.example.demo.exceptions.UserAlreadyExistsException;
+import com.example.demo.mail.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,18 +16,21 @@ public class UserService {
     UserRepository userRepository;
     @Autowired
     BCryptPasswordEncoder passwordEncoder;
-
+    @Autowired
+    EmailService emailService;
     @Autowired
     JwtTokenUtil jwtTokenUtil;
 
     public String saveAndGenerateToken(UserEntity user) throws Exception {
         if (userRepository.findByUserName(user.getUserName()).isPresent() || userRepository.findByEmail(user.getEmail()).isPresent()) {
-            throw new UserAlreadyExistsException("Benutzernamen oder Email bereits vergeben.");
+            throw new UserAlreadyExistsException("Benutzernamen oder Email ist bereits vergeben.");
         }
 
         // Passwort verschlüsseln und Benutzer speichern
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
+
+        emailService.sendRegistrationEmail(user.getEmail(), user.getUserName());
 
         // JWT-Token für den Benutzer generieren und zurückgeben
         return jwtTokenUtil.generateToken(user.getUserName());
