be29226193620902ec62b679a707acc5aa95c511
Efehan Ömer Cekic <Efehan.Cekic@Student.HTW-Berlin.de>
2024-10-03 20:41:00 +0200

    Global CorsConfiguration added

--- src/main/java/com/example/demo/product/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-    Optional<ProductEntity> findByProductName(String productName);
-    Optional<ProductEntity> findAllByProductId(Long productId);
-}
