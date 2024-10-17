239514b198b95143706ab938e8601d4cc0cf2efd
Efehan Ömer Cekic <Efehan.Cekic@Student.HTW-Berlin.de>
2024-10-17 10:19:53 +0200

    jwt token jetzt verschlüsselt + product added (speichern funktioniert nicht richtig)

--- src/main/java/com/example/demo/config/SecurityConfiguration.java
+++ src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -34,7 +34,7 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**", "/product").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
---
+++ src/main/java/com/example/demo/product/ProductController.java
@@ -0,0 +1,20 @@
+package com.example.demo.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+}
